b1de83058b7b3100c4e4d000e517af76f7d39c46
IsakBH <89648535+IsakBH@users.noreply.github.com>
2025-01-27 15:08:26 +0100

    Lagt til ny modus med høyere presisjon og lav hastighet

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ fn main() {
     let mut enigo = Enigo::new();
     let device_state = DeviceState::new();
 
-    println!("Trykk på Meta + LCtrl + LShift + Pil for å bevege musen");
+    println!("Trykk på Meta + LCtrl + LShift + Pil for å bevege musen med lav presisjon");
+    println!("Trykk på Meta + LCtrl + LAlt + Pil for å bevege musen med høy presisjon");
     println!("Trykk på Meta + LAlt + Escape for å gå ut av programmet");
 
     loop {
         let keys: Vec<Keycode> = device_state.get_keys();
 
+        ////////////////////////////////////// VANLIG HASTIGHET / LAVERE PRESISJON ///////////////////////////////////////////////
         // sjekker om brukeren trykker på Windows + LControl + LShift + Høyre pil
         if keys.contains(&Keycode::Meta)
             && keys.contains(&Keycode::LControl)
@@ -21,7 +23,6 @@ fn main() {
             // flytter musen 25 pixeler til høyre
             enigo.mouse_move_relative(25, 0);
         }
-
         // sjekker om brukeren trykker på Windows + LControl + LShift + Venstre pil
         if keys.contains(&Keycode::Meta)
             && keys.contains(&Keycode::LControl)
@@ -31,7 +32,6 @@ fn main() {
             // flytter musen 25 pixeler til venstre
             enigo.mouse_move_relative(-25, 0);
         }
-
         // sjekker om brukeren trykker på Windows + LControl + LShift + Nedover pil
         if keys.contains(&Keycode::Meta)
             && keys.contains(&Keycode::LControl)
@@ -41,7 +41,6 @@ fn main() {
             // flytter musen 25 pixeler nedover
             enigo.mouse_move_relative(0, 25);
         }
-
         // sjekker om brukeren trykker på Windows + LControl + LShift + Nedover pil
         if keys.contains(&Keycode::Meta)
             && keys.contains(&Keycode::LControl)
@@ -52,12 +51,50 @@ fn main() {
             enigo.mouse_move_relative(0, -25);
         }
 
+        ////////////////////////////////////// TREG HASTIGHET / HØYERE PRESISJON ///////////////////////////////////////////////
+        // sjekker om brukeren trykker på Windows + LControl + LShift + Høyre pil
+        if keys.contains(&Keycode::Meta)
+            && keys.contains(&Keycode::LControl)
+            && keys.contains(&Keycode::LAlt)
+            && keys.contains(&Keycode::Right)
+        {
+            // flytter musen 25 pixeler til høyre
+            enigo.mouse_move_relative(10, 0);
+        }
+        // sjekker om brukeren trykker på Windows + LControl + LShift + Venstre pil
+        if keys.contains(&Keycode::Meta)
+            && keys.contains(&Keycode::LControl)
+            && keys.contains(&Keycode::LAlt)
+            && keys.contains(&Keycode::Left)
+        {
+            // flytter musen 25 pixeler til venstre
+            enigo.mouse_move_relative(-10, 0);
+        }
+        // sjekker om brukeren trykker på Windows + LControl + LShift + Nedover pil
+        if keys.contains(&Keycode::Meta)
+            && keys.contains(&Keycode::LControl)
+            && keys.contains(&Keycode::LAlt)
+            && keys.contains(&Keycode::Down)
+        {
+            // flytter musen 25 pixeler nedover
+            enigo.mouse_move_relative(0, 10);
+        }
+        // sjekker om brukeren trykker på Windows + LControl + LShift + Nedover pil
+        if keys.contains(&Keycode::Meta)
+            && keys.contains(&Keycode::LControl)
+            && keys.contains(&Keycode::LAlt)
+            && keys.contains(&Keycode::Up)
+        {
+            // flytter musen 25 pixeler oppover
+            enigo.mouse_move_relative(0, -10);
+        }
+
         // sjekker etter hotkey for å gå ut av scriptet
         if keys.contains(&Keycode::Meta)
             && keys.contains(&Keycode::LAlt)
             && keys.contains(&Keycode::Escape)
         {
-            println!("Exiting...");
+            println!("Vi preikes");
             break;
         }
 
